bebca5fbc7eaf09bd3e6341bee9faead0f5b2b5d
Eduardo Hernandez <ert_1000@hotmail.com>
2023-02-25 14:01:10 -0500

    Ajustes en el Crud de cliente para replica.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
+import { ToastrModule } from "ngx-toastr";
 
 import { AppComponent } from './app.component';
 import { ClientsComponent } from './clients/clients.component';
@@ -9,6 +10,7 @@ import { ReportsComponent } from './reports/reports.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AccountsComponent } from './accounts/accounts.component';
 import { FilterPipe } from "./pipes/filter.pipe";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -21,8 +23,10 @@ import { FilterPipe } from "./pipes/filter.pipe";
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToastrModule.forRoot(), // Toastr Messages
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrService } from "ngx-toastr";
 
 @Component({
   selector: 'app-clients',
@@ -7,7 +8,9 @@ import { Component } from '@angular/core';
 })
 export class ClientsComponent {
 
-   myArray = [
+  constructor(private toastr: ToastrService) {}
+
+   clientsList = [
      {
        "id": 1,
        "name": "Matt",
@@ -18,18 +21,69 @@ export class ClientsComponent {
      }
    ];
 
+  auxiliar: any = {
+    "id": Number,
+    "name": String,
+    "address": String,
+    "phoneNumber": Number,
+    "password": String,
+    "status": Boolean,
+  }
+
+  getData(): void{
+
+
+  }
+
+  showSucces(): void{
+    this.toastr.success('Guardado correctamente','Guardar');
+  }
+
+  showError(): void{
+    this.toastr.error('Se ha eliminado el registro correctamente.','Eliminación');
+  }
+
+
+  saveItem(): String{
+    this.showSucces();
+    return '';
+  }
+
+
+
+
    edition: boolean = false;
    searchFilter: string = "";
 
-   editItem(): void{
+   editItem(itemToEdit: any): void{
      if(this.edition){
        this.edition = false;
      }else{
+       this.auxiliar = Object.assign({},itemToEdit);
        this.edition = true;
+       console.log(this.auxiliar);
      }
   }
 
+  cancelEdit(item: any): void{
+
+    if(this.edition){
+      item = {
+        "name": this.auxiliar.name,
+        "address": this.auxiliar.address,
+        "phoneNumber": this.auxiliar.phoneNumber,
+        "password": this.auxiliar.password,
+        "status": this.auxiliar.status
+      }
+      this.edition = false;
+    }else{
+      this.edition = true;
+      console.log(this.auxiliar);
+    }
+  }
+
   deleteItem(): boolean{
+     this.showError();
      return true;
   }
 
